a9b27e9ae77d761b6e2b0c1c247a45da7a3b3471
Wei Shynn <weishynnsoon@gmail.com>
2022-04-19 21:20:54 +0800

    third-commit

--- module1/src/components/AppHeader.js
+++ module1/src/components/AppHeader.js
@@ -1,86 +1,110 @@
-import React, {useContext} from 'react';
-import {Routes, Route, Link, useHistory } from 'react-router-dom';
-import '../styles/AppHeader.css'
-import AppRoute from '../AppRoutes';
-import { CartContext } from '../contexts/CartContext';
-
+import React, { useContext, useEffect } from "react";
+import { Routes, Route, Link, useHistory } from "react-router-dom";
+import "../styles/AppHeader.css";
+import AppRoute from "../AppRoutes";
+import { CartContext } from "../contexts/CartContext";
+import { LoginContext } from "../contexts/LoginContext";
 
 function AppHeader() {
+  const loginContext = useContext(LoginContext);
+  const {isLoggedIn} = useContext(LoginContext);
+  console.log(isLoggedIn);
 
-    let history = useHistory();
-    const { cartCount } = useContext(CartContext);
-    const logoutUser = () => {
-      
-        history.push("/logout");
-    }
+  let history = useHistory();
+  const { cartCount } = useContext(CartContext);
+  const logoutUser = () => {
+    loginContext.logoutUser();
+    history.push("/logout");
+  };
 
-    return (
-        <div className="app-header">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-header fixed-top">
-                <Link className="navbar-brand" to="/">SmartBank Credit Card</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  return (
+    <div className="app-header">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-header fixed-top">
+        <Link className="navbar-brand" to="/">
+          SmartBank Credit Card
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item navbar-link active"> 
-                           {/* Provide Homepage routing link */}
-                            <Link className="nav-link" to="/">
-                                <i className="fas fa-home"></i>
-                                <span className="sr-only">(current)</span>
-                            </Link>
-                        </li>
-                        <li className="nav-item navbar-link">
-                            {/* Provide Rewards routing link */}
-                            <Link className="btn btn-success" to="/rewards">
-                                Rewards Catalogue
-                            </Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item navbar-link active">
+              {/* Provide Homepage routing link */}
+              <Link className="nav-link" to="/">
+                <i className="fas fa-home"></i>
+                <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item navbar-link">
+              {/* Provide Rewards routing link */}
+              <Link className="btn btn-success" to="/rewards">
+                Rewards Catalogue
+              </Link>
+            </li>
+          </ul>
 
+          {isLoggedIn ? (
+            <div className="header-right">
+              {/* Provide cart routing link */}
+              <Link type="button" className="btn btn-success" to="/cart">
+                <i className="fas fa-shopping-cart" />
+                &nbsp; My Cart &nbsp;
+                <span className="badge badge-light">{cartCount}</span>
+                <span className="sr-only">cart items</span>
+              </Link>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {isLoggedIn ? (
+            <div className="header-right">
+              <Link type="button" className="btn btn-success" to="/">
+                <i className="fas fa-user" />
+                &nbsp; Welcome{" "}
+                {loginContext.loggedInUser.userName == ""
+                  ? " Guest"
+                  : loginContext.loggedInUser.userName}
+                <span className="sr-only">logged in user</span>
+              </Link>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        
+        {
+                        isLoggedIn ? (
+                            <button className="btn btn-dark" onClick={logoutUser}>
+                                <i className="fas fa-sign-out-alt" />&nbsp; Log Out
 
-                        </li>
+                            </button>
+                        )
+                            :
+                            (
+                                <Link className="btn btn-dark" to="/login">
+                                    <i className="fas fa-sign-in-alt" />&nbsp; Sign in
 
+                                </Link>
+                            )
+                    }
+        
+        </div>
+      </nav>
 
-                    </ul>
-
-                    <div className="header-right">
-                        {/* Provide cart routing link */}
-                        <Link type="button" className="btn btn-success" to="/cart">
-                            <i className="fas fa-shopping-cart" />&nbsp; My Cart &nbsp;
-                            <span className="badge badge-light">
-                                {cartCount}
-                            </span>
-                            <span className="sr-only">cart items</span>
-                        </Link>
-                    </div>
-
-                    <div className="header-right">
-
-                        <Link type="button" className="btn btn-success" to="/">
-                            <i className="fas fa-user" />&nbsp; Welcome Guest
-                            <span className="sr-only">logged in user</span>
-                        </Link>
-                    </div>
-
-                    <button className="btn btn-dark" onClick={logoutUser}>
-                        <i className="fas fa-sign-out-alt" />&nbsp; Log Out
-
-                    </button>
-
-                </div>
-            </nav>
-
-            {/* 
+      {/* 
                 Routing configuration to different components
             */}
-            <AppRoute/>
-
-            
-        </div>
-    );
-
+      <AppRoute />
+    </div>
+  );
 }
-
-
 
 export default AppHeader;
--- module1/src/modules/Homepage.js
+++ module1/src/modules/Homepage.js
@@ -1,47 +1,63 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import { LoginContext } from "../contexts/LoginContext";
+import { Link } from "react-router-dom";
 
 function Homepage() {
+  const isLoggedIn = useContext(LoginContext);
+  const loginContext = useContext(LoginContext);
+  console.log(loginContext);
 
-    const loggedInUser = {
-        ccNumber: 0,
-        ccName: "Dummy Card",
-        userName: "Guest User",
-        userId: "guest",
-        availableRedeemPoints: 0,
-        totalRewardsGained: 0
-    }
+  // const loggedInUser = {
+  //     ccNumber: 0,
+  //     ccName: "Dummy Card",
+  //     userName: "Guest User",
+  //     userId: "guest",
+  //     availableRedeemPoints: 0,
+  //     totalRewardsGained: 0
+  // }
 
-
-    return (
-        <div className="homepage container">
-            <h2>Welcome to SmartBank credit card</h2>
-           
-            <div className="user-detail">
-                <div>
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Credit Card Number</th>
-                                <th>Card Name</th>
-                                <th>User Name</th>
-                                <th>Available Points</th>
-                                <th>Total Rewards Gained (<i className="fas fa-rupee-sign"></i>)</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>ccNumber</td>
-                                <td>ccName</td>
-                                <td>userName</td>
-                                <td>availableRedeemPoints</td>
-                                <td>totalRewardsGained</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  return (
+    <div className="homepage container">
+      <h2>Welcome to SmartBank credit card</h2>
+      {isLoggedIn ? (
+        <div className="user-detail">
+          <div>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Credit Card Number</th>
+                  <th>Card Name</th>
+                  <th>User Name</th>
+                  <th>Available Points</th>
+                  <th>
+                    Total Rewards Gained (<i className="fas fa-rupee-sign"></i>)
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{loginContext.loggedInUser.ccNumber}</td>
+                  <td>{loginContext.loggedInUser.ccName}</td>
+                  <td>{loginContext.loggedInUser.userName}</td>
+                  <td>{loginContext.loggedInUser.availableRedeemPoints}</td>
+                  <td>{loginContext.loggedInUser.totalRewardsGained}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
+      ) : (
+        <div className="mt-5">
+          Please Sign in to redeem your points. &nbsp; &nbsp;
+          <Link className="btn btn-primary" to="/login">
+            <i className="fas fa-user" />
+            &nbsp; Sign in
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Homepage; 
+export default Homepage;
